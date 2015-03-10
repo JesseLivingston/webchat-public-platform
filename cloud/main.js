c5400f279d6a095a71e29ac12d5816c2dd54a5f1
Jesse Chen <livingston.chen@gmil.com>
2015-03-10 21:59:32 +0800

    add weixin-bot.

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,27 @@
-require("cloud/app.js");
+// require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
-AV.Cloud.define("hello", function(request, response) {
+/*AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
+});*/
+
+var express = require("express");
+var webot = require("weixin-robot");
+
+var app = express();
+
+webot.set("hi", "你好！");
+
+webot.set("subscribe", {
+	pattern: function(info) {
+		return info.is("event") && info.param.event === "subscribe";
+	},
+	handler: function(info) {
+		return "欢迎订阅！";
+	}
 });
+
+webot.watch(app, {
+	token: "634c5e6fe5904fd0b052d62a247dbb07",
+	path: "wechatapi"
+})
